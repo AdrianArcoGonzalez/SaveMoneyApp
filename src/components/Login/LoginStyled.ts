8d898e35b8819d27795117129127c7ebe9cd42e9
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-02 12:53:39 +0100

    add login component style and login page component style

---
+++ src/components/Login/LoginStyled.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+const LoginStyled = styled.div`
+  font-family: "Roboto";
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    &-label {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 5px;
+      padding: 5px;
+    }
+
+    &-input {
+      font-family: inherit;
+      text-align: center;
+      padding: 5px;
+    }
+  }
+
+  .link-register {
+    text-decoration: none;
+    color: #3070ad;
+    font-weight: bolder;
+  }
+
+  .text-login {
+    font-weight: bolder;
+    padding: 5px;
+  }
+
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+  }
+  .errors {
+    text-align: center;
+    color: #ff3131;
+    font-weight: bold;
+    font-size: 0.7rem;
+  }
+`;
+
+export default LoginStyled;
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,19 @@
+import Login from "../../components/Login/Login";
+import LoginPageStyled from "./LoginPageStyled";
+
+const LoginPage = (): JSX.Element => {
+  return (
+    <LoginPageStyled>
+      <div className="secondary-title-container">
+        <h2 className="secondary-title">
+          Save your time and your
+          <span className="secondary-title--blue"> money</span>
+        </h2>
+      </div>
+      <h3 className="third-title">Welcome!</h3>
+      <Login />
+    </LoginPageStyled>
+  );
+};
+
+export default LoginPage;
