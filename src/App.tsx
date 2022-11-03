e3805b81bfd63583ba524d06f8ef3effdde1b8bf
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-03 13:22:33 +0100

    deleted register folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import Header from "./components/Header/Header";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 const App = (): JSX.Element => {
   return (
@@ -12,7 +11,6 @@ const App = (): JSX.Element => {
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </>
   );
