96f01a0a88b00a05e7cb6693b719205236a72223
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-08 13:48:52 +0100

    add refactor to login formic components and tests

---
+++ src/components/LoginFormikForm/LoginFormikForm.test.tsx
@@ -0,0 +1,35 @@
+import { screen } from "@testing-library/react";
+import renderWithFormik from "../../Utils/test-utils";
+import LoginFormikForm from "./LoginFormikForm";
+
+describe("Given a LoginFormikForm componen", () => {
+  describe("When it's instantiated", () => {
+    test("Then it should show 2 inputs and the login button", () => {
+      const labelUser = "Username";
+      const labelPassword = "Password";
+      const buttonText = "Login";
+
+      renderWithFormik(<LoginFormikForm />);
+
+      const inputUser = screen.getByLabelText(labelUser);
+      const inputPassword = screen.getByLabelText(labelPassword);
+      const button = screen.getByRole("button");
+
+      expect(inputUser).toBeInTheDocument();
+      expect(inputPassword).toBeInTheDocument();
+      expect(button).toHaveTextContent(buttonText);
+    });
+
+    test("And a text with a link to navigate to register", () => {
+      const textAccount = "Haven't an account yet?";
+      const linkText = "Register";
+      renderWithFormik(<LoginFormikForm />);
+
+      const text = screen.getByText(textAccount);
+      const link = screen.getByRole("link");
+
+      expect(text).toBeInTheDocument();
+      expect(link).toHaveTextContent(linkText);
+    });
+  });
+});
---
+++ src/components/LoginFormikForm/LoginFormikFormStyled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const LoginFormikFormStyled = styled.div`
+  .text-register {
+    font-weight: bolder;
+    padding: 5px;
+  }
+  .link-register {
+    text-decoration: none;
+    color: #3070ad;
+    font-weight: bolder;
+  }
+`;
+
+export default LoginFormikFormStyled;
