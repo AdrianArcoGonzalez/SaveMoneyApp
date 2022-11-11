d5cd2451d11e80bc458060e82b2ca7f427c8c992
Ivan Garcia <ivanggv2@gmail.com>
2022-11-11 14:11:49 +0100

    created component Register

---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,42 @@
+import { Formik } from "formik";
+import { ErrorsHandler } from "../../interfaces/interfaces";
+import { validateRegisterSchema } from "../../schemas/validateRegisterSchema";
+import { RegisterFormikForm } from "../RegisterFormikForm/RegisterFormikForm";
+import RegisterStyled from "./RegisterStyled";
+
+export const Register = (): JSX.Element => {
+  const initialValues = {
+    username: "",
+    password: "",
+    confirmPassword: "",
+    email: "",
+    confirmEmail: "",
+  };
+
+  return (
+    <RegisterStyled>
+      <Formik
+        initialValues={initialValues}
+        validate={(values) => {
+          const errors: ErrorsHandler = {};
+
+          if (values.confirmPassword !== values.password) {
+            errors.confirmPassword = "The passwords must be the same";
+          }
+
+          if (values.confirmEmail !== values.email) {
+            errors.confirmEmail = "The email must be the same";
+          }
+
+          return errors;
+        }}
+        validationSchema={validateRegisterSchema}
+        onSubmit={(values, { resetForm }) => {
+          resetForm();
+        }}
+      >
+        <RegisterFormikForm />
+      </Formik>
+    </RegisterStyled>
+  );
+};
