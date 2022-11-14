10a2f3905a829b09aed8389e22cb42d0b2e40c54
Ivan Garcia <ivanggv2@gmail.com>
2022-11-14 12:21:12 +0100

    refactored to have an array instead a line pro input

--- src/components/Register/Register.test.tsx
+++ src/components/Register/Register.test.tsx
@@ -1,44 +1,23 @@
-import { screen } from "@testing-library/react";
+import { act, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import renderWithFormik from "../../Utils/test-utils";
 import { Register } from "./Register";
 
 describe("Given a Register components", () => {
-  const inputUserText = "Username";
-  const inputPasswordText = "Password";
-  const inputRepeatPasswordText = "Repeat password";
-  const inputEmailText = "Email";
-  const inputRepeatEmailText = "Repeat email";
-
-  const userWrite = "John";
-  const passwordWrite = "12345678";
-  const emailWrite = "john@mail.com";
-
   describe("When it's instantiated", () => {
     test("Then it should show five inputs what update the own status when the user type", async () => {
+      const userWrite = "Something";
+
       renderWithFormik(<Register />);
 
-      const inputUser = screen.getByLabelText(inputUserText);
-      const inputPassword = screen.getByLabelText(inputPasswordText);
-      const inputRepeatPassword = screen.getByLabelText(
-        inputRepeatPasswordText
-      );
-      const inputEmail = screen.getByLabelText(inputEmailText);
-      const inputRepeatEmail = screen.getByLabelText(inputRepeatEmailText);
-      const button = screen.getByRole("button");
+      const inputs = await screen.findAllByRole("textbox");
 
-      await userEvent.type(inputUser, userWrite);
-      await userEvent.type(inputPassword, passwordWrite);
-      await userEvent.type(inputRepeatPassword, passwordWrite);
-      await userEvent.type(inputEmail, emailWrite);
-      await userEvent.type(inputRepeatEmail, emailWrite);
-
-      expect(button).toBeInTheDocument();
-      expect(inputUser).toHaveValue(userWrite);
-      expect(inputPassword).toHaveValue(passwordWrite);
-      expect(inputRepeatPassword).toHaveValue(passwordWrite);
-      expect(inputEmail).toHaveValue(emailWrite);
-      expect(inputRepeatEmail).toHaveValue(emailWrite);
+      await act(async () => {
+        inputs.forEach(async (input) => {
+          await userEvent.type(input, userWrite);
+          expect(input).toHaveValue(userWrite);
+        });
+      });
     });
   });
 });
