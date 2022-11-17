5c08b9fcab6729c690cd498026b99fbfedbfef64
Ivan Garcia <ivanggv2@gmail.com>
2022-11-17 11:39:02 +0100

    added ToastContainer to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,25 @@ import LandingPage from "./pages/LandingPage/LandingPage";
 import Header from "./components/Header/Header";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import { ToastContainer, Zoom } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = (): JSX.Element => {
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Zoom}
+      />
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
