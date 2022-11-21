0c35c62b47ab14471f16fa2b39ee0b150ce4bc1c
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-21 18:18:15 +0100

    add main page jsx

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,7 +3,13 @@ import Card from "../../components/Card/Card";
 const MainPage = (): JSX.Element => {
   return (
     <div className="card">
-      <Card income={2000} moneySaved={4000} spend={1000} currencyType={"$"} />
+      <Card
+        income={2000.14}
+        moneySaved={8000.49}
+        spend={1000.34}
+        currencyType={"$"}
+      />
+      <Card limit={1000} spend={700} currencyType={"$"} />
     </div>
   );
 };
