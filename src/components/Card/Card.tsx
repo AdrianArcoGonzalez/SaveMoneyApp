ba0c3b03c7c0914cc5e1e8b73cdb6a5cf2f50a8f
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-21 18:18:44 +0100

    refactor card to condicional render

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,10 @@
 import CardStyled from "./CardStyled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 interface CardProps {
-  moneySaved: number;
-  income: number;
+  moneySaved?: number;
+  income?: number;
   spend: number;
   limit?: number | null;
   currencyType: "€" | "$";
@@ -20,17 +21,17 @@ const Card = ({
     <CardStyled>
       {limit ? (
         <div className="card-limit">
-          <h2>Limit for this month</h2>
-          <span>
+          <h2 className="card-title">Limit for this month</h2>
+          <span className="card-main-info">
             {currencyType}
             {limit}
           </span>
-
-          <span>grafico</span>
-
-          <span>
-            {spend}/{limit}
-          </span>
+          <div className="spend-grafic">
+            <span>grafico</span>
+            <span className="spend-limit">
+              {spend}/{limit}
+            </span>
+          </div>
         </div>
       ) : (
         <div className="card-money-saved">
@@ -40,9 +41,8 @@ const Card = ({
           </span>
 
           <div className="money-stats">
-            <section className="money-stats-specific">
-              <FontAwesomeIcon icon={"arrow-up-long"} />
-
+            <section className="money-stats-income">
+              <FontAwesomeIcon icon={faArrowUp} className="arrow-up" />
               <div className="money-income">
                 <span className="money-income-title">Income</span>
                 <span className="money-income">
@@ -50,14 +50,16 @@ const Card = ({
                 </span>
               </div>
             </section>
-            <div>
-              <span>Spend</span>
-              <div>
+
+            <section className="money-stats-spend">
+              <FontAwesomeIcon icon={faArrowUp} className="arrow-down" />
+              <div className="money-spend">
+                <span className="money-spend-title">Spend</span>
                 <span>
                   {currencyType} {spend}
                 </span>
               </div>
-            </div>
+            </section>
           </div>
         </div>
       )}
