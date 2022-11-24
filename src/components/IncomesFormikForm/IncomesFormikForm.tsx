9c47b748601f620ad0d2df63ecac173b880e5dc0
Ivan Garcia <ivanggv2@gmail.com>
2022-11-24 17:08:43 +0100

    added title, intro text and modified class names

--- src/components/IncomesFormikForm/IncomesFormikForm.tsx
+++ src/components/IncomesFormikForm/IncomesFormikForm.tsx
@@ -1,47 +1,55 @@
 import { Field, Form, useFormikContext } from "formik";
+import UserContext from "../../store/UserContext/UserContext";
 import Button from "../Button/Button";
+import { useContext } from "react";
 
 import CustomInputField from "../CustomInputField/CustomInputField";
 import { IncomesFormikFormStyled } from "./IncomesFormikFormStyled";
+import mockUser from "../../Utils/mockBack";
 
 export const IncomesFormikForm = () => {
   let { isValid } = useFormikContext();
+  const { user } = useContext(UserContext);
 
   return (
     <IncomesFormikFormStyled>
       <Form noValidate autoComplete="off" className="form-container">
+        <h1 className="form-container__title">{`Welcome ${user.userName}`}</h1>
+        <span className="form-container__intro-text">
+          Before starting we need to to ask some questions
+        </span>
+
         <CustomInputField
           label="Monthly incomes?"
           name="incomes"
-          class="form-input"
+          class="form-container__input"
           placeholder="1500"
           type="number"
         />
 
         <div className="form-container__radio-group">
-          <label id="my-radio-group">Choose your currency</label>
-          <div role="group" aria-labelledby="my-radio-group">
-            <Field
-              className="form-radio"
-              type="radio"
-              name="currency"
-              value="€"
-            />
-            €
-            <Field
-              className="form-radio"
-              type="radio"
-              name="currency"
-              value="$"
-            />
-            $
+          <label id="my-radio-group" className="radio-group__label">
+            Choose your currency
+          </label>
+          <div
+            role="group"
+            aria-labelledby="my-radio-group"
+            className="radio-group__radio-buttons"
+          >
+            <label htmlFor="€">
+              <Field id="€" type="radio" name="currency" value="€" />€
+            </label>
+
+            <label htmlFor="$">
+              <Field id="$" type="radio" name="currency" value="$" />$
+            </label>
           </div>
         </div>
 
         <CustomInputField
           label="Wich is your savings target?"
           name="saving"
-          class="form-input"
+          class="form-container__input"
           placeholder="25000"
           type="number"
         />
