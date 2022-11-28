e6f5a962097529074bdce5d29ee832e12484d949
Ivan Garcia <ivanggv2@gmail.com>
2022-11-28 14:10:09 +0100

    added expenses and incomes to the user

--- src/Utils/mockBack.ts
+++ src/Utils/mockBack.ts
@@ -1,4 +1,5 @@
 import { ExpenseIncome, UserMoneyData } from "../interfaces/interfaces";
+import { categories, incomeIcon } from "./categories";
 
 export const mockUser: UserMoneyData = {
   userName: "test",
@@ -6,33 +7,57 @@ export const mockUser: UserMoneyData = {
   isLogged: true,
   currency: "$",
   incomes: [
-    { name: "work", quantity: 2000, date: "24-11-2022" },
-    { name: "work extra payment", quantity: 1000, date: "24-11-2022" },
-    { name: "Sell clothes", quantity: 180, date: "24-11-2022" },
-    { name: "Won in casino", quantity: 2000, date: "24-11-2022" },
+    {
+      name: "work",
+      quantity: 2000,
+      date: "2022-11-01",
+      category: "Income",
+      icon: incomeIcon,
+    },
+    {
+      name: "work extra payment",
+      quantity: 1000,
+      date: "2022-11-15",
+      category: "Income",
+      icon: incomeIcon,
+    },
+    {
+      name: "Sell clothes",
+      quantity: 180,
+      date: "2022-11-23",
+      category: "Income",
+      icon: incomeIcon,
+    },
+    {
+      name: "Won in casino",
+      quantity: 2000,
+      date: "2022-11-28",
+      category: "Income",
+      icon: incomeIcon,
+    },
   ],
   moneySaved: 8000,
   expenses: [
     {
       name: "Zara",
       quantity: 300,
-      date: "24-11-2022",
-      icon: "",
-      category: "Clothes",
+      date: "2022-11-27",
+      icon: categories[2].icon,
+      category: categories[2].name,
     },
     {
       name: "Mercadona",
       quantity: 200,
-      date: "22-11-2022",
-      icon: "",
-      category: "Food",
+      date: "2022-11-24",
+      icon: categories[0].icon,
+      category: categories[0].name,
     },
     {
       name: "Rent",
       quantity: 500,
-      date: "22-11-2022",
-      icon: "",
-      category: "Home",
+      date: "2022-11-28",
+      icon: categories[4].icon,
+      category: categories[4].name,
     },
   ],
   savingTarget: 1000,
