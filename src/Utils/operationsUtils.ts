1cdeae3f3d361407c2dda5127b2b13011f626558
Ivan Garcia <ivanggv2@gmail.com>
2022-11-28 14:09:45 +0100

    added the new interface ExpenseIncome

--- src/Utils/operationsUtils.ts
+++ src/Utils/operationsUtils.ts
@@ -1,7 +1,7 @@
-import { ExpenseIncome, Income } from "../interfaces/interfaces";
+import { ExpenseIncome } from "../interfaces/interfaces";
 
 export const getLimitToWaste = (
-  incomes: Income[],
+  incomes: ExpenseIncome[],
   savingTarget: number
 ): number => {
   let initialIncomes = 0;
@@ -13,7 +13,7 @@ export const getLimitToWaste = (
   return totalIncomes - savingTarget;
 };
 
-export const getTotalIncomes = (incomes: Income[]): number => {
+export const getTotalIncomes = (incomes: ExpenseIncome[]): number => {
   let initialIncome = 0;
   const totalIncomes = incomes.reduce(
     (accumulator, incomes) => accumulator + incomes.quantity,
