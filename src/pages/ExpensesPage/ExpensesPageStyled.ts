8c04833015cb68f7b49b37ab8f647f39f71c6342
Ivan Garcia <ivanggv2@gmail.com>
2022-11-29 11:44:37 +0100

    created component ExpensesPage

---
+++ src/pages/ExpensesPage/ExpensesPageStyled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const ExpensesPageStyled = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .main-title {
+    font-size: 1rem;
+    align-self: flex-start;
+  }
+`;
+
+export default ExpensesPageStyled;
