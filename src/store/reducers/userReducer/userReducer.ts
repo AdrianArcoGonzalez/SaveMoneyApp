db9f456beb7e67618290fa7b1ef967c2383b9196
Ivan Garcia <ivanggv2@gmail.com>
2022-11-29 23:41:43 +0100

    modified user with the new interface UserLogged

--- src/store/reducers/userReducer/userReducer.ts
+++ src/store/reducers/userReducer/userReducer.ts
@@ -1,11 +1,12 @@
 import { UserLoged } from "../../../interfaces/interfaces";
+import { initialUser } from "../../../Utils/mockBack";
 import { Action, LoginAction, UnknownAction } from "../../types/actions";
 
 const userReducer = (
   previousUser: UserLoged,
   action: Action | UnknownAction
 ) => {
-  let user: UserLoged = { userName: "", token: "", isLogged: false };
+  let user = initialUser;
 
   if (action.type === "login") {
     user = {
@@ -13,6 +14,11 @@ const userReducer = (
       userName: (action as LoginAction).payload.userName,
       token: (action as LoginAction).payload.token,
       isLogged: (action as LoginAction).payload.isLogged,
+      currency: (action as LoginAction).payload.currency,
+      expenses: (action as LoginAction).payload.expenses,
+      incomes: (action as LoginAction).payload.incomes,
+      moneySaved: (action as LoginAction).payload.moneySaved,
+      savingTarget: (action as LoginAction).payload.savingTarget,
     };
   } else {
     user = { ...previousUser };
