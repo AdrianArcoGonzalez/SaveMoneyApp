9c2090eec39181c989b48bbb4579485e10ffbe68
Ivan Garcia <ivanggv2@gmail.com>
2022-12-13 18:11:48 +0100

    modified the ui object and added the new actions

--- src/store/reducers/uiReducer/uiReducer.ts
+++ src/store/reducers/uiReducer/uiReducer.ts
@@ -1,20 +1,37 @@
-import { Theme } from "../../uiContext/uiContext";
+import { Ui } from "../../../interfaces/interfaces";
 import { Action, ThemeAction, UnknownAction } from "../../types/actions";
 
-export const uiReducer = (
-  previousTheme: "light" | "dark",
-  action: Action | UnknownAction
-) => {
-  let theme: Theme;
+export const uiReducer = (previousUi: Ui, action: Action | UnknownAction) => {
+  let ui: Ui = {
+    theme: "light",
+    showExpenseForm: false,
+    showIncomeForm: false,
+  };
 
   switch (action.type) {
     case "toggleTheme":
-      theme = (action as ThemeAction).payload;
+      ui = { ...previousUi, theme: (action as ThemeAction).payload };
+      break;
+
+    case "openExpenseForm":
+      ui = { ...previousUi, showExpenseForm: true };
+      break;
+
+    case "closeExpenseForm":
+      ui = { ...previousUi, showExpenseForm: false };
+      break;
+
+    case "openIncomeForm":
+      ui = { ...previousUi, showIncomeForm: true };
+      break;
+
+    case "closeIncomeForm":
+      ui = { ...previousUi, showIncomeForm: false };
       break;
 
     default:
-      theme = previousTheme;
+      ui = { ...previousUi };
       break;
   }
-  return theme;
+  return ui;
 };
