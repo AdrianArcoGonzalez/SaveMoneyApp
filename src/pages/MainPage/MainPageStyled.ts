b62297e6c00f78e8322b1970b41e1ddbc15ab860
CSarroca <sarroca93@gmail.com>
2022-12-13 18:07:19 +0100

    changed min-height for pages

--- src/pages/MainPage/MainPageStyled.ts
+++ src/pages/MainPage/MainPageStyled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const MainPageStyled = styled.div`
   width: 100%;
+  min-height: 725px;
   display: flex;
   flex-direction: column;
   justify-content: center;
