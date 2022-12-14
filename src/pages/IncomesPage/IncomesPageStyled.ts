427c94422155d42b0db0b3ad2d26d3417d29b882
Ivan Garcia <ivanggv2@gmail.com>
2022-12-14 14:19:05 +0100

    added new style for the button

--- src/pages/IncomesPage/IncomesPageStyled.ts
+++ src/pages/IncomesPage/IncomesPageStyled.ts
@@ -6,9 +6,26 @@ const IncomesPageStyled = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
   .main-title {
+    width: 100%;
     font-size: 1rem;
+    display: flex;
     align-self: flex-start;
+    justify-content: space-between;
+    align-items: center;
+
+    &__button {
+      margin: 0;
+      font-size: 2rem;
+      background-color: transparent;
+      border: none;
+      padding: 0;
+    }
+
+    &__button:hover {
+      color: blue;
+    }
   }
 `;
 
