b32ee09f783857b9bf0c9a580e452d8210917eef
Ivan Garcia <ivanggv2@gmail.com>
2022-12-15 16:54:45 +0100

    added function handleClixk and two types of component

--- src/components/Incomes/Incomes.tsx
+++ src/components/Incomes/Incomes.tsx
@@ -1,27 +1,52 @@
 import { Formik } from "formik";
-import { IncomesValues } from "../../interfaces/interfaces";
+import { useContext } from "react";
+import { IncomesValues, UserLoged } from "../../interfaces/interfaces";
 import { validateIncomesForm } from "../../schemas/validateIncomesForm";
+import { userLoginActionCreator } from "../../store/actions/userActions/userActions";
+import UserContext from "../../store/UserContext/UserContext";
 import { IncomesFormikForm } from "../IncomesFormikForm/IncomesFormikForm";
 
-const initialValues: IncomesValues = {
-  incomes: 0,
-  saving: 0,
-  currency: "$",
-};
+interface IncomeProps {
+  type: "initial" | "update";
+  onClick?: () => void;
+}
+export const Incomes = ({ type, onClick }: IncomeProps): JSX.Element => {
+  const { user, dispatch } = useContext(UserContext);
 
-export const Incomes = (): JSX.Element => {
-  const handleSubmit = () => {};
+  const initialValues: IncomesValues =
+    type === "update"
+      ? {
+          incomes: user.monthlyIncomes,
+          saving: user.savingTarget,
+          currency: "$",
+        }
+      : {
+          incomes: 0,
+          saving: 0,
+          currency: "$",
+        };
+
+  const handleSubmit = (values: IncomesValues) => {
+    const newUser: UserLoged = {
+      ...user,
+      currency: values.currency,
+      savingTarget: values.saving,
+      monthlyIncomes: values.incomes,
+    };
+
+    dispatch(userLoginActionCreator(newUser));
+  };
 
   return (
     <Formik
       onSubmit={(values, { resetForm }) => {
-        handleSubmit();
-        resetForm({ values: initialValues });
+        handleSubmit(values);
+        resetForm();
       }}
       initialValues={initialValues}
       validationSchema={validateIncomesForm}
     >
-      <IncomesFormikForm />
+      <IncomesFormikForm type={type} onClick={onClick} />
     </Formik>
   );
 };
