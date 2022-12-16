ac3f99c443b9194a7b3d6c83d217c205e10ce83e
CSarroca <sarroca93@gmail.com>
2022-12-16 17:48:35 +0100

    added test for userActions cases

--- src/store/actions/userActions/userActions.test.ts
+++ src/store/actions/userActions/userActions.test.ts
@@ -1,6 +1,10 @@
 import { mockUser } from "../../../Utils/mockBack";
-import { LoginAction } from "../../types/actions";
-import { userLoginActionCreator } from "./userActions";
+import { LoginAction, NewExpenseIncomeAction } from "../../types/actions";
+import {
+  newExpenseActionCreator,
+  newIncomeActionCreator,
+  userLoginActionCreator,
+} from "./userActions";
 
 describe("Given a userLoginActionCreator", () => {
   describe("When it's invoked with a userData", () => {
@@ -12,6 +16,38 @@ describe("Given a userLoginActionCreator", () => {
       };
 
       const actionReturned = userLoginActionCreator(userData);
+
+      expect(actionReturned).toStrictEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a newExpenseActionCreator", () => {
+  describe("When it's invoked with an expense", () => {
+    test("Then it should return an action with the correct type and payload", () => {
+      const expense = mockUser.expenses[0];
+      const expectedAction: NewExpenseIncomeAction = {
+        type: "newExpense",
+        payload: expense,
+      };
+
+      const actionReturned = newExpenseActionCreator(expense);
+
+      expect(actionReturned).toStrictEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a newIncomeActionCreator", () => {
+  describe("When it's invoked with an income", () => {
+    test("Then it should return an action with the correct type and payload", () => {
+      const income = mockUser.expenses[0];
+      const expectedAction: NewExpenseIncomeAction = {
+        type: "newIncome",
+        payload: income,
+      };
+
+      const actionReturned = newIncomeActionCreator(income);
 
       expect(actionReturned).toStrictEqual(expectedAction);
     });
