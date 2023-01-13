eb628181c976cf3668246853b5b66d2afa2ca10f
Ivan Garcia <ivanggv2@gmail.com>
2023-01-13 17:55:18 +0100

    added functions to create and update movements and tested

--- src/hooks/useMovements/useMovements.ts
+++ src/hooks/useMovements/useMovements.ts
@@ -1,8 +1,12 @@
 import { useContext } from "react";
-import { MovementsType } from "../../interfaces/interfaces";
+import { ExpenseIncome, MovementsType } from "../../interfaces/interfaces";
 import {
   deleteExpenseActionCreator,
   deleteIcomeActionCreator,
+  newExpenseActionCreator,
+  newIncomeActionCreator,
+  updateExpenseActionCreator,
+  updateIncomeActionCreator,
 } from "../../store/actions/userActions/userActions";
 import UserContext from "../../store/UserContext/UserContext";
 import { useFeedback } from "../useFeedback/useFeedback";
@@ -10,6 +14,51 @@ import { useFeedback } from "../useFeedback/useFeedback";
 export const useMovements = () => {
   const { dispatch } = useContext(UserContext);
   const { successFeedback, errorFeedback } = useFeedback();
+
+  const createMovement = async (
+    movement: ExpenseIncome,
+    type: "Income" | "Expense"
+  ) => {
+    try {
+      //await axios.post(environments.update, movement);
+
+      if (!movement.id) {
+        movement.id = `${movement.date}${movement.name}`;
+      }
+
+      const action =
+        type === "Income"
+          ? newIncomeActionCreator(movement)
+          : newExpenseActionCreator(movement);
+
+      dispatch(action);
+      successFeedback("Movement created successfully");
+      return true;
+    } catch (error) {
+      errorFeedback("Can´t create the movement, please try again");
+      return false;
+    }
+  };
+
+  const updateMovement = async (
+    movement: ExpenseIncome,
+    type: "Income" | "Expense"
+  ) => {
+    try {
+      //await axios.post(environments.update, movement, {params: { id: movement.id },});
+      const action =
+        type === "Income"
+          ? updateIncomeActionCreator(movement)
+          : updateExpenseActionCreator(movement);
+
+      dispatch(action);
+      successFeedback("Movement updated successfully");
+      return true;
+    } catch (error) {
+      errorFeedback("Can´t update the movement, please try again");
+      return false;
+    }
+  };
 
   const deleteMovement = async (movementName: string, type: MovementsType) => {
     if (type === "Recent movements") {
@@ -30,5 +79,5 @@ export const useMovements = () => {
     }
   };
 
-  return { deleteMovement };
+  return { createMovement, updateMovement, deleteMovement };
 };
